e2bcc24928d8c2accbc5c90cf456a19250b3a42f
flo <florian.hauser95@gmail.com>
2026-01-31 11:07:03 +0100

    add static files and update script

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@ cd /srv/django/gfm && \
 source .venv/bin/activate && \
 git fetch origin && git checkout main && git pull && \
 pip install -r requirements.txt && \
-python manage.py migrate && \
-python manage.py collectstatic --noinput && \
+python manage.py migrate --settings=config.settings.prod && \
+python manage.py collectstatic --noinput --settings=config.settings.prod && \
 sudo systemctl restart django-gunicorn && \
 curl -I http://127.0.0.1:8001/gfm/
 
